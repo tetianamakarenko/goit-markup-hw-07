fd1dbee2191b395627e7fa882024d4eb57cbb9a2
Tetiana Makarenko <m3t7a9@gmail.com>
2023-02-02 20:28:03 +0100

    5 save

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,7 +3,7 @@
         openMenuBtn: document.querySelector('[data-menu-open]'),
         closeMenuBtn: document.querySelector('[data-menu-close]'),
         menu: document.querySelector('[data-menu]'),
-        body: document.querySelector('[body]'),
+        body: document.querySelector('body'),
     };
 
     refs.openMenuBtn.addEventListener('click', toggleMenu);
