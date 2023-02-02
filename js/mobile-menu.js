11144997c9cf56b9b4826400f21d9960936a2fae
Tetiana Makarenko <m3t7a9@gmail.com>
2023-02-02 15:38:18 +0100

    2 save

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,16 @@
 (() => {
     const refs = {
-      openMenuBtn: document.querySelector("[data-menu-open]"),
-      closeMenuBtn: document.querySelector("[data-menu-close]"),
-      Menu: document.querySelector("[data-menu]"),
+        openMenuBtn: document.querySelector('[data-menu-open]'),
+        closeMenuBtn: document.querySelector('[data-menu-close]'),
+        menu: document.querySelector('[data-menu]'),
+        body: document.querySelector('[body]'),
     };
-  
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
-  
+
+    refs.openMenuBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
     function toggleMenu() {
-      refs.Menu.classList.toggle("is-hidden");
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
     }
-  })();
+})();
